c5ab64921fb965fcdc6a5050753cb65b7fc6dffa
SCOU7 <thp0116@gmail.com>
2022-07-06 17:27:30 +0900

    deleting unnecessary build files leaving Makefile as the last one

--- Compile_Test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ --verbose -dynamiclib -o /Users/Dev/workspace_Rambert/project_Rambert/src/Rambert/libapplib.dylib /Users/Dev/workspace_Rambert/project_Rambert/src/Rambert/Application.cpp
